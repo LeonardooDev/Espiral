eb670432a5c001f179fbacfd443f8c3f30448ad1
LeooDevvv <elleoduran007@gmail.com>
2026-02-26 20:29:00 -0800

    Pueba 4 de commit en comentario

--- codigos/codigo_ia.py
+++ codigos/codigo_ia.py
@@ -8,6 +8,8 @@ from huggingface_hub import InferenceClient
 
 # Prueba 3 
 
+# Prueba 4
+
 load_dotenv() # Esto carga el archivo .env
 TOKEN_HF = os.getenv("TOKEN_HF") # Lee la variable sin mostrarla en el código
 
